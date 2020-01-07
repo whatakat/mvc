eab4e13dca8685b48a008830a56b90d9a7fcd729
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-07 13:53:06 +0300

    refactor BeatModelInterface

--- src/com/company/model/BeatModelInterface.java
+++ src/com/company/model/BeatModelInterface.java
@@ -7,7 +7,8 @@ public interface BeatModelInterface {
     void initialize();
     void on();
     void off();
-    void setBPM();
+    void setBPM(int bpm);
+    int getBPM();
     void registerObserver(BeatObserver o);
     void removeObserver(BeatObserver o);
     void registerObserver(BPMObserver o);
